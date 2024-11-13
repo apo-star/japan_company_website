e5c07be1b525cda0513263cd570c7bb92a148388
apo-star <apolle.world@gmail.com>
2024-11-13 10:39:35 -0800

    feat: install threejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
 import './App.css';
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <Canvas
+      style={{
+        width: '100vw',
+        height: '100vh',
+        background: '#f0f8ff'
+      }}
+      camera={{
+        fov: 75,
+        near: 0.1,
+        far: 200,
+        position: [10, 4, 10]
+      }}>
+      <OrbitControls makeDefault />
+      <mesh>
+        <sphereGeometry />
+        <meshNormalMaterial wireframe />
+      </mesh>
+    </Canvas>
+  );
 }
 
 export default App;
